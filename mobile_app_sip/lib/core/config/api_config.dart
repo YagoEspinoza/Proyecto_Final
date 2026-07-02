c9a0ab7e55e58814675cc60802b1abeebaa659ca
Yago Espinoza <74250016@continental.edu.pe>
2026-07-02 12:43:38 -0500

    Errores solucionados

--- mobile_app_sip/lib/core/config/api_config.dart
+++ mobile_app_sip/lib/core/config/api_config.dart
@@ -1,10 +1,15 @@
-import 'dart:io';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'dart:io' show Platform;
 
 class ApiConfig {
+  // URL de tu backend ya desplegado en Render
+  static const String prodBaseUrl = 'https://banco-backend-56el.onrender.com';
+
   static const String devBaseUrl = 'http://127.0.0.1:8003';
   static const String emulatorBaseUrl = 'http://10.0.2.2:8003';
 
   static String get baseUrl {
+    if (kIsWeb) return prodBaseUrl;
     if (Platform.isAndroid) return emulatorBaseUrl;
     return devBaseUrl;
   }
